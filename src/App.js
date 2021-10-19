778168913f74226c4338bfe2e28570b7cb99b62d
Fernando De Leon <gasnare@hotmail.com>
2021-10-19 12:28:17 -0500

    Add more special character and getting data for firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import { InputForm } from "./components/InputForm";
 import { db } from "./firebase-config";
 
 const App = () => {
+  const [inputs, setInputs] = useState([]);
+  const [inputCharacters, setInputCharacters] = useState([]);
+  const [inputTexts, setInputTexts] = useState([]);
+
+  const getInputNumbers = async () => {
+    db.collection("numbers").onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setInputs(docs);
+    });
+  };
+
+  const getInputCharacters = async () => {
+    db.collection("characters").onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setInputCharacters(docs);
+    });
+  };
+
+  const getInputTexts = async () => {
+    db.collection("text").onSnapshot((querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setInputTexts(docs);
+    });
+  };
+
+  useEffect(() => {
+    getInputNumbers();
+    getInputCharacters();
+    getInputTexts();
+  }, []);
+
   const findSpecialChart = (str) => {
-    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç,.:;'/'?][{}=+",
       to = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
       mapping = {};
 
@@ -22,7 +63,7 @@ const App = () => {
   };
 
   const findChart = (str) => {
-    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç,.:;'/'?][{}=+",
       to = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
       mapping = {};
 
@@ -45,19 +86,28 @@ const App = () => {
     var input = inputObject.input;
     var firstInput = input.charAt(0);
     var lastChar = input[input.length - 1];
+    var specialChar = findSpecialChart(input);
+    var number = parseInt(input);
     var objectInput = {
-      input: input,
+      text: input,
       firstCharInput: firstInput,
       lastCharInput: lastChar,
     };
-    var a = findSpecialChart(input);
-    console.log("findChar", findChart(input));
-    console.log("findSpecialChar: ", input == a);
-    // await db.collection("inputs").doc().set(objectInput);
-    console.log("Objeto: ", objectInput);
-    console.log("Primer caracter", firstInput);
-    console.log("Ultimo caracter", lastChar);
-    console.log("New input added");
+    var tableCharacter = {
+      character: findChart(input),
+    };
+    var numbers = {
+      number: number,
+    };
+
+    // eslint-disable-next-line eqeqeq
+    if (number == input) {
+      await db.collection("numbers").doc().set(numbers);
+    } else if (specialChar === input) {
+      await db.collection("text").doc().set(objectInput);
+    } else {
+      await db.collection("characters").doc().set(tableCharacter);
+    }
   };
 
   return (
@@ -65,9 +115,39 @@ const App = () => {
       <div className="row">
         <InputForm addTask={addTask} />
       </div>
-      <div>
-        <p>El texto es:</p>
-        <p></p>
+      <div className="row">
+        <div className="col-md-4">
+          <h2>Numbers</h2>
+          {inputs.map((input) => (
+            <div key={input.id} className="card mb-1">
+              <div className="card-body">
+                <h4>Number: {input.number}</h4>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="col-md-4">
+          <h2>Characters</h2>
+          {inputCharacters.map((input) => (
+            <div key={input.id} className="card mb-1">
+              <div className="card-body">
+                <h4>Special character {input.character}</h4>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="col-md-4">
+          <h2>Texts</h2>
+          {inputTexts.map((input) => (
+            <div key={input.id} className="card mb-1">
+              <div className="card-body">
+                <h5>Text: {input.text}</h5>
+                <p>First character: {input.firstCharInput}</p>
+                <p>Last character: {input.lastCharInput}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
